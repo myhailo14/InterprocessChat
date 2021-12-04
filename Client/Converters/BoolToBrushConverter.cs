c2f18a1d72942f2a62dd8d209bf9beb7d4a60dbc
myhailo14 <mykhailo.vovkanych@outlook.com>
2021-12-04 15:02:27 +0200

    Design added

--- Client/Converters/BoolToBrushConverter.cs
+++ Client/Converters/BoolToBrushConverter.cs
@@ -6,8 +6,8 @@ namespace SocketChat.Converters
 {
     public class BoolToBrushConverter : IValueConverter
     {
-        private readonly Brush _okBrush = new SolidColorBrush(Color.FromArgb(255, 192, 255, 192));
-        private readonly Brush _errorBrush = new SolidColorBrush(Color.FromArgb(255, 255, 192, 192));
+        private readonly Brush _okBrush = new SolidColorBrush(Color.FromArgb(255, 73, 123, 72));
+        private readonly Brush _errorBrush = new SolidColorBrush(Color.FromArgb(255, 254, 65, 55));
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
